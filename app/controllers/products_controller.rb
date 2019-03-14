c07caf1bff83470277264bbddb00022c37f70927
yonwari <nozo.matsumoto1105@gmail.com>
2019-03-14 10:18:15 +0000

    Add product create,index,show features

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,27 @@
 class ProductsController < ApplicationController
+
   def index
+    # @random_products = Product.order("RANDOM()")
+    @random_products = Product.page(params[:page]).per(9).order("RANDOM()")
   end
 
   def show
+    @product = Product.find_by(id: params[:id])
   end
 
   def new
+    @product = Product.new
+    @song = Song.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "商品の登録に成功しました"
+      redirect_to products_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,4 +29,13 @@ class ProductsController < ApplicationController
 
   def update
   end
+
+  # def search
+  #   @products = Product.search(params[:id])
+  # end
+
+  private
+    def product_params
+      params.require(:product).permit(:product_title, :product_price, :label_id, :genre_id, :explanation,:image, :stock)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/new'
+  get 'songs/edit'
   root 'roots#top'
   get '/about', to: 'roots#about'
   get '/admin_top', to: 'roots#admin_top'
@@ -9,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :products, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:create]
 
   resources :labels, only: [:index, :new, :create, :edit, :update]
   resources :artists, only: [:index, :new, :create, :edit, :update]
