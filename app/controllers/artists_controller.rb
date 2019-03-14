cb5201bd13448f9bca80cc8934080cb6df7bdaa2
youkunman <s.y.92.jr@gmail.com>
2019-03-14 04:06:10 +0000

    Add kaminari config

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,16 +1,29 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.page(params[:page]).reverse_order
   end
 
   def new
+    @artist = Artist.new
   end
 
   def create
+    artist = Artist.new(artist_params)
+    artist.save
+    redirect_to "/artists"
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    artist = Artist.find(params[:id])
+    artsit.update(artist_params)
+    redirect_to "/artists"
+  end
+
+  def artist_params
+    params.require(:artist).permit(:artist_name)
   end
 end
