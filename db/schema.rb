531500e2e75bc671a3bca221c0327bfd6a760f17
shirosum <shirosumsum@gmail.com>
2019-03-16 08:08:04 +0000

    Add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_090757) do
+ActiveRecord::Schema.define(version: 2019_03_14_060929) do
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2019_03_13_090757) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
-    t.datetime "orders_date"
     t.integer "status"
     t.string "destination_name"
     t.string "destination_postcode"
@@ -73,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_090757) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "order_date"
   end
 
   create_table "products", force: :cascade do |t|
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2019_03_13_090757) do
     t.string "postcode"
     t.string "address"
     t.string "phone_number"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
