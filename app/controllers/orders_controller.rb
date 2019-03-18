8e74e503db97d5da78e4172888f857c3270c2c66
yonwari <nozo.matsumoto1105@gmail.com>
2019-03-18 08:45:20 +0000

    Add order show edit index feature

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,21 +5,45 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @user = User.find(@order.user_id)
+    @order_item = Product.find(@order.product_id)
   end
 
   def create
-    @order.user.cart_id = current_user.id
-    @order.save
-    redirect_to carts_thanks_path
+    order = Order.new
+
+    # レコード追加処理
+    order.user_id = 1
+    order.status = 10
+    order.destination_name = params[:destination_name]
+    order.destination_postcode = params[:destination_postcode]
+    order.destination_address = params[:destination_address]
+    order.product_id = params[:product_id]
+    order.quantity = params[:quantity]
+    order.total_price = params[:total_price]
+    
+    if @order.save
+
+      # cart_productのレコード削除
+
+      redirect_to carts_thanks_path
+    else
+      flash[:danger] = "注文処理に失敗しました"
+      redirect_to products_path
+    end
+    
   end
 
   def edit
     @order = Order.find(params[:id])
+    @user = User.find(@order.user_id)
+    @order_item = Product.find(@order.product_id)
   end
 
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      flash[:notice] = "ステータスを更新しました"
       redirect_to order_path(@order.id)
     else
       render :edit
@@ -27,8 +51,8 @@ class OrdersController < ApplicationController
   end
 
   private
-  def order_params
-    params.require(:order).permit(:user_id, :destination_id, :product_id,  :status, :quantity, :total_price)
-  end
+    def order_params
+      params.require(:order).permit(:status)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_03_14_060929) do
-=======
-ActiveRecord::Schema.define(version: 2019_03_13_122734) do
->>>>>>> develop
 
   create_table "artists", force: :cascade do |t|
     t.string "artist_name"
@@ -76,7 +72,6 @@ ActiveRecord::Schema.define(version: 2019_03_13_122734) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "order_date"
   end
 
   create_table "products", force: :cascade do |t|
