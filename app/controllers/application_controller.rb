35329778e18b1e3e773fcfeab30275dfef1f686c
kouta0706 <rurukouta76@gmail.com>
2019-03-19 05:29:36 +0000

    Add shiraishi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
-#     before_action :configure_permitted_parameters, if: :devise_controller?
- 
-#   protected
- 
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.for(:sign_up) << :name
-#   end
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters
+    def after_sign_in_path_for(resource)
+      products_path # ログイン後に遷移するpathを設定
+    end
+  
+    def after_sign_out_path_for(resource)
+      new_user_session_path # ログアウト後に遷移するpathを設定
+    end
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name_kana]) 
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    end
 end
--- db/migrate/20190313090757_add_column_to_users.rb
+++ db/migrate/20190313090757_add_column_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_name, :string
-    add_column :users, :user_name_kana, :string
+    add_column :users, :name, :string
+    add_column :users, :name_kana, :string
     add_column :users, :postcode, :string
     add_column :users, :address, :string
     add_column :users, :phone_number, :string
