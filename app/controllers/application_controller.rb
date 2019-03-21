90dde54d1895de503137dd00bef194a72346dd36
kouta0706 <rurukouta76@gmail.com>
2019-03-21 04:03:55 +0000

    Addshiraishi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     
     def after_sign_in_path_for(resource)
         if current_user
-        products_path 
+            products_path 
         else
             admin_top_path
         end
