ea2ded8d10d991066f4d4f442c324aa2393751cc
shirosum <shirosumsum@gmail.com>
2019-03-25 05:14:12 +0000

    shaka

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,11 +1,10 @@
 class RootsController < ApplicationController
   before_action :authenticate_admin,only: [:admin_top]
   def top
-  	@random_product = Product.order("RANDOM()").first
+  	#@random_product = Product.order("RANDOM()").first
   end
   def about
   end
-
   def admin_top
   end
   def shakashaka
