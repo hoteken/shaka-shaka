9821dd1543a61df6d5dcbd338b7bedf0447f891e
shirosum <shirosumsum@gmail.com>
2019-03-26 02:05:26 +0000

    modify songsErrorMessages

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,17 +1,16 @@
-$(function() {
-	console.log("sssss")
- 		$('#language-wrapper').hover(
-    function() {
-    	$('.language-text').fadeIn();
-    },
-    function() {
-    	$('.language-text').fadeOut();
-    }
-  	);
-		$('.mount').css({
-  		fontSize: '20pt',
-  		'color': 'white',
-  		'background-color': '#1e90ff'
-});
-});
-
+// $(function() {
+// 	console.log("sssss")
+//  		$('#language-wrapper').hover(
+//     function() {
+//     	$('.language-text').fadeIn();
+//     },
+//     function() {
+//     	$('.language-text').fadeOut();
+//     }
+//   	);
+// 		$('.mount').css({
+//   		fontSize: '20pt',
+//   		'color': 'white',
+//   		'background-color': '#1e90ff'
+// });
+// });
