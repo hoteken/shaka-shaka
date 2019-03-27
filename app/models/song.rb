3410cba3e9ee7d2ad863ed5c0b3934f4d9833ff7
yonwari <nozo.matsumoto1105@gmail.com>
2019-03-27 12:43:28 +0000

    [Fix]song model validate

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,6 @@ class Song < ApplicationRecord
   belongs_to :product
 
   validates :song_title, presence: true
+  validates :track_order, presence: true
+  validates :disk_number, presence: true
 end
