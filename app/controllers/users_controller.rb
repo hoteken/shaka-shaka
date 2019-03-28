aa6870a16d8d84aaeee7d26b25d33b3bc8b28c19
shirosum <shirosumsum@gmail.com>
2019-03-28 05:13:01 +0000

    modify usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,17 @@ class UsersController < ApplicationController
   before_action :authenticate_user!,only: [:index,:show]
   before_action :authenticate_admin,only: [:index]
   before_action :authenticate_adm_or_correct_user, only: [:show, :edit, :update]
-  
+
   def index
     @user = current_user
     @users = User.page(params[:page]).reverse_order
-    @search = User.ransack(params[:q])  
-    @result = @search.result.page(params[:page])     
-  
+    @search = User.ransack(params[:q])
+    @result = @search.result.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
     @orders = @user.orders.reverse_order
-    
   end
 
   def edit
@@ -31,11 +29,14 @@ class UsersController < ApplicationController
     end
   end
   def destroy
-    @user = User.find(params[:id]) 
-    @user.destroy
-    redirect_to users_path
+    @user = User.find(params[:id])
+    if @user.destroy
+      flash[:notice] = "ユーザーの削除に成功しました"
+      redirect_to users_path
+    end
   end
-  private 
+
+  private
 
   def user_params
     params.require(:user).permit(:user_name, :user_name_kana, :postcode, :address, :phone_number,:email)
